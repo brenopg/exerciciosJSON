334f69486c9632c83db22f17684bceaf77f20b4b
Breno <breno.parreira@live.com>
2016-03-31 22:19:07 -0300

    Carregando imagens e títulos pelo JSON

--- exe4/js/script.js
+++ exe4/js/script.js
@@ -6,6 +6,9 @@ $.ajax({
 
 			for(i in data.noticias){		
 			$('#not' + i + '').append('<p>' + data.noticias[i].noticia + '</p>');
+			$('#img' + i + '').append('<img src="' + data.noticias[i].src + '">');	
+			$('#nome' + i + '').append('<p>' + data.noticias[i].nome + '</p>');
+
 		}
 		   
 		},
